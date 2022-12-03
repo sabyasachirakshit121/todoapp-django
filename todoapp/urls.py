c3cc42c55f7b4ac30f6685bddfc89373232030fc
sabyasachirakshit <sabyasachirakshitacs19@gmail.com>
2022-12-03 19:21:51 +0530

    chanegs reverted

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,33 +1,28 @@
+"""todoapp URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-
-
-schema_view = get_schema_view(
-    openapi.Info(
-        title="PushNotification API",
-        default_version='v1',
-        description="Test description",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@snippets.local"),
-        license=openapi.License(name="BSD License"),
-    ),
-    public=True,
-    permission_classes=[permissions.AllowAny],
-)
-
+from todoapp.views import todoappView, addTodoView, updateTodoView, deleteTodoView, demo
 
 urlpatterns = [
-    path('account/', include('account.urls', namespace='app_account')),
     path('admin/', admin.site.urls),
-    path('events/', include('events.urls', namespace='app_events')),
-    path('swagger(<format>\.json|\.yaml)', schema_view.without_ui(
-        cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger',
-         cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc',
-         cache_timeout=0), name='schema-redoc'),
-
+    path('', include('api.urls')),
+    path('todoapp/', todoappView),
+    path('demo/', demo, name='demo'),
+    path('addTodoItem/', addTodoView),
+    path('updateTodoItem/<int:i>/', updateTodoView),
+    path('deleteTodoItem/<int:i>/', deleteTodoView),
 ]
