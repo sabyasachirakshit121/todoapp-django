c7a99ee2ebbc8973033569701c15338286904e25
sabyasachirakshit <sabyasachirakshitacs19@gmail.com>
2022-12-03 12:10:46 +0530

    debugging

--- todoapp/calendar_API.py
+++ todoapp/calendar_API.py
@@ -2,20 +2,22 @@ from decouple import config
 from google.oauth2 import service_account
 import googleapiclient.discovery
 import datetime
+import json
 
-CAL_ID = config('CAL_ID')
+
+CAL_ID = "104a92a3cd198c2062645e570737318d05c2dfb2f1361e64b743a4b0e223de66@group.calendar.google.com"
 SCOPES = ['https://www.googleapis.com/auth/calendar']
-SERVICE_ACCOUNT_FILE = '../google-credentials.json'
+SERVICE_ACCOUNT_FILE = json.load(open('google-credentials.json'))
 
 
 def test_calendar():
     print("RUNNING TEST_CALENDAR()")
-
     credentials = service_account.Credentials.from_service_account_file(
         SERVICE_ACCOUNT_FILE, scopes=SCOPES)
     service = googleapiclient.discovery.build(
         'calendar', 'v3', credentials=credentials)
 
+    print(service)
     # CREATE A NEW EVENT
     new_event = {
         'summary': "New Task 1",
@@ -43,7 +45,7 @@ def test_calendar():
 
         print(e)
 
-    #uncomment the following lines to delete each existing item in the calendar
+    # uncomment the following lines to delete each existing item in the calendar
     #event_id = e['id']
         # service.events().delete(calendarId=CAL_ID, eventId=event_id).execute()
 
