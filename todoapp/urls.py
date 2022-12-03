52620134d8b606c44085f0d477b04a5de26561d7
sabyasachirakshit <sabyasachirakshitacs19@gmail.com>
2022-12-03 11:15:08 +0530

    google calendar added

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from todoapp.views import todoappView, addTodoView, updateTodoView, deleteTodoView
+from todoapp.views import todoappView, addTodoView, updateTodoView, deleteTodoView, demo
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('api.urls')),
     path('todoapp/', todoappView),
+    path('demo/', demo, name='demo'),
     path('addTodoItem/', addTodoView),
     path('updateTodoItem/<int:i>/', updateTodoView),
     path('deleteTodoItem/<int:i>/', deleteTodoView),
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from api.models import Task
 from django.http import HttpResponseRedirect 
+from .calendar_API import test_calendar
 
 def todoappView(request):
     all_task_items = Task.objects.all()
@@ -22,3 +23,9 @@ def deleteTodoView(request, i):
     y = Task.objects.get(id= i)
     y.delete()
     return HttpResponseRedirect('/todoapp/') 
+
+
+def demo(request):
+    results = test_calendar()
+    context = {"results": results}
+    return render(request, 'demo.html', context)
